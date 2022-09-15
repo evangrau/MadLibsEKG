d3103d03f9782c74bb49b766e6907587c87f690d
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-09-15 13:34:17 -0500

    working on getting input from each edittext

--- app/src/main/java/com/example/madlibsekg/MainActivity.java
+++ app/src/main/java/com/example/madlibsekg/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.madlibsekg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goNext(View view) {
+        Intent intent = new Intent(this, MadLibsActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
